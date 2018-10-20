7f416450ef2b51aae2b283f998157e707dc504db
Alex <31661440+am2367@users.noreply.github.com>
2018-10-20 18:25:48 -0400

    pop density function

---
+++ server/SpaceAppsServices/optimalPopDensitySort.py
@@ -0,0 +1,14 @@
+import constants
+
+#sort cities by population density relative to optimal density
+def optimalPopDensitySort(cityList):
+    
+    cityToDensity = {}
+
+    for city in cityList:
+        diff = abs(city['populationDensity'] - constants.OPTIMAL_DENSITY)
+        cityToDensity[city['city']] = diff
+    
+    sortedCities = sorted(cityToDensity, key=cityToDensity.get)
+
+    print(sortedCities)
