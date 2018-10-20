96e93192cf7c3d19295e6768b9b6ef2601a30fd9
Khulan Myagmardorj <myagmar2@illinois.edu>
2018-10-20 16:22:13 -0400

    refactor long line

--- server/SpaceAppsServices/google_maps.py
+++ server/SpaceAppsServices/google_maps.py
@@ -17,7 +17,10 @@ def getDistance(originCoordinates, destinationCoordinates):
             value (long): distance in meters
         }
     """
-    url = constants.GOOGLE_MAPS_DISTANCE_API + "?" + constants.GOOGLE_MAPS_KEY_URI + "&origins=" + originCoordinates + "&destinations=" + destinationCoordinates
+    url = constants.GOOGLE_MAPS_DISTANCE_API \
+        + "?" + constants.GOOGLE_MAPS_KEY_URI \
+        + "&origins=" + originCoordinates \
+        + "&destinations=" + destinationCoordinates
     response = requests.get(url)
     if (response.status_code == 200):
         data = response.json()
