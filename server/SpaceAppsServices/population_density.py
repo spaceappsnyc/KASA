9033687109695983cd57641a281b03489626709b
Khulan Myagmardorj <myagmar2@illinois.edu>
2018-10-20 18:45:13 -0400

    refactor and adjust idp coordinates

--- server/SpaceAppsServices/population_density.py
+++ server/SpaceAppsServices/population_density.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import json
 
+import constants
+
 def readDataForPopulationDensity(populationFileName):
     """
     Should only need to be run once to generate
@@ -17,3 +19,18 @@ def readDataForPopulationDensity(populationFileName):
         p["populationDensity"] = popDensity
     with open('./resources/populationWithDensities.json', 'w') as fp:
         json.dump(populations, fp)
+
+def optimalPopDensitySort():
+    """
+    sort cities by population density relative to optimal density
+    """
+    with open("./resources/populationWithDensities.json") as f:
+        cityList = json.load(f)
+
+    cityToDensity = {}
+
+    for city in cityList:
+        diff = abs(city['populationDensity'] - constants.OPTIMAL_DENSITY)
+        cityToDensity[city['city']] = diff
+    
+    return sorted(cityToDensity, key=cityToDensity.get)
