5d9a3c7592e2725b2c658ae4eb1731e65dcc7609
Sneh95 <snehmunshi95@gmail.com>
2018-10-20 15:21:59 -0400

    sample get response api

--- server/SpaceAppsServices/healthcare.py
+++ server/SpaceAppsServices/healthcare.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 def readDataForHealthcare(healthcareFileName):
     headers = ["city", "rating"]
@@ -6,4 +7,20 @@ def readDataForHealthcare(healthcareFileName):
     return df.to_json(orient='records')
     #print(df['rating'])
 
-readDataForHealthcare("./resources/test.csv")
+def getUserCoordinates():
+    try:
+		r = requests.get(url = "http://192.168.43.15:4200/api/getUsername")
+		r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        return {'error': 'Error in request'}
+
+	if (r.status_code == 204):
+		return {'error': 'None Found'}
+
+    print("returned data")
+    data = r.json()
+    print(data)
+
+
+#readDataForHealthcare("./resources/test.csv")
+getUserCoordinates()
