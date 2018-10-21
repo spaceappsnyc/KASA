c9bf6315432778eb4303ab587becc95051218909
Alex <31661440+am2367@users.noreply.github.com>
2018-10-20 23:25:15 -0400

    ui dir changes

--- ui/App.js
+++ ui/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import Login from './screens/Login.js';
 
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false,
+    isLoadingComplete: false
   };
 
   render() {
@@ -17,7 +18,8 @@ export default class App extends React.Component {
           onFinish={this._handleFinishLoading}
         />
       );
-    } else {
+    }
+    else {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
