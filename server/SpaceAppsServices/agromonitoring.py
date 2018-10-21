47115c7673d04e56cdd0e3746f4b2c43461d02bf
Khulan Myagmardorj <myagmar2@illinois.edu>
2018-10-21 03:31:59 -0400

    create polygons for top cities

--- server/SpaceAppsServices/agromonitoring.py
+++ server/SpaceAppsServices/agromonitoring.py
@@ -1,30 +1,39 @@
 import json
 import requests
+import time
 
 import constants
 
-def createPolygon(city, coordinates):
-    with open("./resources/polygon_payload_base.json") as f:
-        payload = json.load(f)
-    polygonIds = {}
-    url = constants.AGROMONITORING_POLYGON_API + "?appid=" + constants.AGROMONITORING_API_KEY
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
-    if (response.status_code == 201):
-        data = response.json()
-        polygonIds[city] = data["id"]
-    else:
-        raise Exception("POST request failed: " + url)
+def createPolygon():
+    with open("./resources/polygon_coordinates_payload_closed.json") as f:
+        polygonPayload = json.load(f)
+    polygonIds = []
+    counter = 1
+    skip = 0
+    length = len(polygonPayload)
+    for p in polygonPayload:
+        city = p["name"]
+        print(city)
+        print(str(counter) + ' out of ' + str(length))
+        coordinates = p["geo_json"]["geometry"]["coordinates"]
+
+        if (len(coordinates) > 0):
+            url = constants.AGROMONITORING_POLYGON_API + "?appid=" + constants.AGROMONITORING_API_KEY
+            headers = {'Content-Type': 'application/json'}
+            response = requests.post(url, data=json.dumps(p), headers=headers)
+            print(response.status_code)
+            if (response.status_code == 201):
+                data = response.json()
+                print('SUCCESS: ' + data["id"])
+                temp = {}
+                temp[city] = data["id"]
+                polygonIds.append(temp)
+            else:
+                skip += 1
+                print("skipping: " + city + " " + str(skip))
+        counter += 1
+        time.sleep(5)
     with open('./resources/polygon_ids.json', 'w') as fp:
         json.dump(polygonIds, fp)
 
-createPolygon(
-    "lagos",
-    [
-        [-121.1958,37.6683],
-        [-121.1779,37.6687],
-        [-121.1773,37.6792],
-        [-121.1958,37.6792],
-        [-121.1958,37.6683]
-    ]
-)
+createPolygon()
