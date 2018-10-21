153ff8a75ae79e9c5f2eb809cc07da8035c810a0
Sneh95 <snehmunshi95@gmail.com>
2018-10-20 20:53:18 -0400

    cleaned up score calculation

--- server/SpaceAppsServices/score_calculation.py
+++ server/SpaceAppsServices/score_calculation.py
@@ -1,39 +1,48 @@
 from un_clusters import *
-import json
-
 from population_density import optimalPopDensityScore
+import json
 
 """
     Given a latitude and longitude we calculate the scores for each potential
     displacement camp location based on measures such as distance from UN clusters,
-    distance from healthcare facilities
+    distance from healthcare facilities, accessibility score and population density
 """
 def score_calculation(userCoordinates):
-    unClusterScore = getDistancesToIdpCamps(userCoordinates)
-    print(str(type(unClusterScore)))
-    shortestDistance = unClusterScore[0]["distance"]
-    for unClusterScoreJson in unClusterScore:
-        if (unClusterScoreJson["distance"] < shortestDistance):
-            shortestDistance = unClusterScoreJson["distance"]
-    print("shortest distance is " + str(shortestDistance))
-    print(unClusterScore)
+    #Score Calculation - Based on Distance from IDP Camps
+    unClusterDistances = getDistancesToIdpCamps(userCoordinates)
 
-    unClusterScoreList = []
-    popDensityScores = optimalPopDensityScore()
-    for unClusterScoreJson in unClusterScore:
-        city = unClusterScoreJson["city"]
-        latitude = unClusterScoreJson["latitude"]
-        longitude = unClusterScoreJson["longitude"]
-        existingDistance = unClusterScoreJson["distance"]
-        nearbyIdpCamp = unClusterScoreJson["name"]
-        percentageDiff = (existingDistance - shortestDistance)/shortestDistance
-        unScore = 100 - percentageDiff
-        popDensityScore = popDensityScores[city]["score"]
+    #Get shortest distance IDP Camp value
+    shortestDistance = unClusterDistances[0]["distance"]
+    for unClusterDistancesJson in unClusterDistances:
+        if (unClusterDistancesJson["distance"] < shortestDistance):
+            shortestDistance = unClusterDistancesJson["distance"]
+
+    overallScoreList = []
+
+    #Score Calculation - Based on Population density scores
+    populationDensityScores = optimalPopDensityScore()
+
+    #Iterate through the list of IDP camps and combine scores from each measure
+    for unClusterDistancesJson in unClusterDistances:
+        city = unClusterDistancesJson["city"]
+        latitude = unClusterDistancesJson["latitude"]
+        longitude = unClusterDistancesJson["longitude"]
+        existingDistance = unClusterDistancesJson["distance"]
+        nearbyIdpCamp = unClusterDistancesJson["name"]
+
+        #IDP Camp score
+        percentageIDPCampDiff = (existingDistance - shortestDistance)/shortestDistance
+        unScore = 100 - percentageIDPCampDiff
+
+        #Population Density score
+        popDensityScore = populationDensityScores[city]["score"]
+
+        #Overall Score - taking weights into account
         score = (unScore * 0.6) + (popDensityScore * 0.4)
-        print(score)
         scoreDict = {"city": city, "nearbyIdpCamp": nearbyIdpCamp, "latitude": latitude, "longitude": longitude, "score": score}
-        unClusterScoreList.append(scoreDict)
-    print(unClusterScoreList)
-    return json.dumps(unClusterScoreList)
+        overallScoreList.append(scoreDict)
+
+    print(overallScoreList)
+    return json.dumps(overallScoreList)
 
 score_calculation("6.5244,3.3792")
