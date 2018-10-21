88266740a329e564aa21a365bb554c95bec0ed12
Khulan Myagmardorj <myagmar2@illinois.edu>
2018-10-20 21:17:01 -0400

    generate top 100 city distance from user

--- server/SpaceAppsServices/accessibility.py
+++ server/SpaceAppsServices/accessibility.py
@@ -1,1 +1,26 @@
-#def topFiveClosest(lat, lng):
+import pandas as pd
+import json
+from pprint import pprint
+
+from google_maps import getDistance
+
+def readDataForTopCities(topCitiesFileName):
+    with open(topCitiesFileName) as f:
+        data = json.load(f)
+    return data
+
+def getDistanceFromTopCities(userCoordinates):
+    topCities = readDataForTopCities("./resources/top100CitiesNigeria.json")
+    for topCity in topCities:
+        lat = str(topCity["latitude"])
+        lon = str(topCity["longitude"])
+        topCityCoordinates = lat + ',' + lon
+        distance = ""
+        try:
+            distance = getDistance(userCoordinates, topCityCoordinates)["value"]
+        except Exception as err:
+            print("Error getting distance: " + repr(err))
+        topCity["distanceFromUser"] = distance
+        
+    with open('./resources/top_100_cities_with_distance.json', 'w') as fp:
+        json.dump(topCities, fp)
